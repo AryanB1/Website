928b3561a9b690063bc1cf1eace63c04167f3b3a
AryanB1 <62898176+AryanB1@users.noreply.github.com>
2023-09-02 17:32:16 -0600

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/matrix-356024_1920.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "A series of random lines and numbers on a screen.";
 
 /**
  * Project list
